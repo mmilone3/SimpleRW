a1d7561d4c30b29a60091718d0bea1f6c7915bf7
mmilone <michael.milone2@gmail.com>
2022-04-18 02:10:26 -0400

    Initial project commit
    Input/Output file processing via console. Use parallel processing for files >= 1mb

---
+++ SimpleRW/SimpleRW/SimpleRW.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SimpleRW
+{
+    public class SimpleRW
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string fileInputPath = @"C:\\Users\\Ivan\\Desktop\\New Text Document Input.txt";
+                string fileOutputPath = @"C:\\Users\\Ivan\\Desktop\\New Text Document Output.txt";
+                long fileLength = new FileInfo(fileInputPath).Length;
+#if !DEBUG
+                Console.WriteLine("Input file: ");
+                fileInputPath = Console.ReadLine();
+                Console.WriteLine("Output file: ");
+                fileOutputPath = Console.ReadLine();
+                fileLength = new FileInfo(fileInputPath).Length;
+#endif
+                Console.WriteLine("Processing read/write...");
+                var useParallel = fileLength >= 1000000;
+                if (useParallel)
+                {
+                    ConcurrentQueue<string> data = new ConcurrentQueue<string>();
+                    using (StreamReader r = new StreamReader(fileInputPath))
+                    {
+                        while (!r.EndOfStream)
+                        {
+                            data.Enqueue(r.ReadLine());
+                        }
+                    }
+
+                    using (StreamWriter w = new StreamWriter(fileOutputPath, true, System.Text.Encoding.UTF8))
+                    {
+                        w.AutoFlush = true;
+                        Parallel.For(0, data.Count, (i) =>
+                        {
+                            if (data.TryDequeue(out string result))
+                            {
+                                w.WriteLine(result);
+                            }
+                        });
+                    }
+                }
+                else
+                {
+                    using (StreamReader r = new StreamReader(fileInputPath))
+                    {
+                        using (StreamWriter w = new StreamWriter(fileOutputPath, true, System.Text.Encoding.UTF8))
+                        {
+                            w.AutoFlush = true;
+                            while (!r.EndOfStream)
+                            {
+                                w.WriteLine(r.ReadLine());
+                            }
+                        }
+                    }
+                }
+
+                Console.WriteLine("Done");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadKey();
+            }
+        }
+    }
+}
