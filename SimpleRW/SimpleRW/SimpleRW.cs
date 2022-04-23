44c0b9dc69beabe01898b784179d6835f1a35ce2
michael.milone2 <michael.milone2@gmail.com>
2022-04-23 00:49:14 -0400

    process multiple input files

--- SimpleRW/SimpleRW/SimpleRW.cs
+++ SimpleRW/SimpleRW/SimpleRW.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SimpleRW
@@ -11,58 +12,69 @@ namespace SimpleRW
         {
             try
             {
-                string fileInputPath, fileOutputPath;
-                long fileLength;
-#if DEBUG
-                fileInputPath = @"C:\\Users\\Ivan\\Desktop\\New Text Document Input.txt";
-                fileOutputPath = @"C:\\Users\\Ivan\\Desktop\\New Text Document Output.txt";
-                fileLength = new FileInfo(fileInputPath).Length;
-#else
-                Console.WriteLine("Input file: ");
-                fileInputPath = Console.ReadLine();
-                Console.WriteLine("Output file: ");
-                fileOutputPath = Console.ReadLine();
-                fileLength = new FileInfo(fileInputPath).Length;
-#endif
-                Console.WriteLine("Processing read/write...");
-                var useParallel = fileLength >= 1000000;
-                if (useParallel)
-                {
-                    ConcurrentQueue<string> data = new ConcurrentQueue<string>();
-                    using (StreamReader r = new StreamReader(fileInputPath))
-                    {
-                        while (!r.EndOfStream)
-                        {
-                            data.Enqueue(r.ReadLine());
-                        }
-                    }
+                Console.WriteLine("Input file paths(ex. filePath1, filePath2): ");
+                string[] fileInputPaths = Console.ReadLine().Split(",").Select(item => item.Trim()).ToArray();
+                string[] fileOutputPaths = new string[fileInputPaths.Length];
+                Task[] tasks = new Task[fileInputPaths.Length];
 
-                    using (StreamWriter w = new StreamWriter(fileOutputPath, true, System.Text.Encoding.UTF8))
-                    {
-                        w.AutoFlush = true;
-                        Parallel.For(0, data.Count, (i) =>
-                        {
-                            if (data.TryDequeue(out string result))
-                            {
-                                w.WriteLine(result);
-                            }
-                        });
-                    }
-                }
-                else
+                for (int i = 0; i < fileInputPaths.Length; i++)
                 {
-                    using (StreamReader r = new StreamReader(fileInputPath))
+                    var indexOfExt = fileInputPaths[i].LastIndexOf(".");
+                    var outputFile = fileInputPaths[i].Substring(0, indexOfExt) + "_Output." + fileInputPaths[i].Substring(indexOfExt + 1);
+                    fileOutputPaths[i] = outputFile;
+                }
+
+                Console.WriteLine("Processing read/write...");
+                for (int i = 0; i < fileInputPaths.Length; i++)
+                {
+                    var fileInputPath = fileInputPaths[i];
+                    var fileOutputPath = fileOutputPaths[i];
+                    var fileLength = new FileInfo(fileInputPath).Length;
+
+                    var task = Task.Factory.StartNew(() =>
                     {
-                        using (StreamWriter w = new StreamWriter(fileOutputPath, true, System.Text.Encoding.UTF8))
+                        var useParallel = fileLength >= 1000000;
+                        if (useParallel)
                         {
-                            w.AutoFlush = true;
-                            while (!r.EndOfStream)
+                            ConcurrentQueue<string> data = new ConcurrentQueue<string>();
+                            using (StreamReader r = new StreamReader(fileInputPath))
                             {
-                                w.WriteLine(r.ReadLine());
+                                while (!r.EndOfStream)
+                                {
+                                    data.Enqueue(r.ReadLine());
+                                }
+                            }
+
+                            using (StreamWriter w = new StreamWriter(fileOutputPath, true, System.Text.Encoding.UTF8))
+                            {
+                                w.AutoFlush = true;
+                                Parallel.For(0, data.Count, (i) =>
+                                {
+                                    if (data.TryDequeue(out string result))
+                                    {
+                                        w.WriteLine(result);
+                                    }
+                                });
                             }
                         }
-                    }
+                        else
+                        {
+                            using (StreamReader r = new StreamReader(fileInputPath))
+                            {
+                                using (StreamWriter w = new StreamWriter(fileOutputPath, true, System.Text.Encoding.UTF8))
+                                {
+                                    w.AutoFlush = true;
+                                    while (!r.EndOfStream)
+                                    {
+                                        w.WriteLine(r.ReadLine());
+                                    }
+                                }
+                            }
+                        }
+                    });
+                    tasks[i] = task;
                 }
+                Task.WaitAll(tasks);
 
                 Console.WriteLine("Done");
                 Console.ReadKey();
